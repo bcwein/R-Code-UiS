61dc9713916063551ed6fd6476b4df9e303ac9b2
bjorncw <bjorncw@stud.ntnu.no>
2020-08-18 13:22:15 +0200

    Added lecture code

--- Lectures/R-programming-1.R
+++ Lectures/R-programming-1.R
@@ -4,3 +4,32 @@ rm(list = ls())
 a <- 2 != 1
 print(a)
 
+if(a) {
+  print("First")
+} else {
+  print("Second")
+}
+
+for (i in c(1, 2, 3, 6, 9)) {
+  print(i)
+}
+
+for (i in 1:10) {
+  print(i)
+  if(i >= 5){
+    break
+  }
+}
+
+x <- rep(0.0, 10)
+y <- rep(0.0, 10)
+
+for (i in 1:10) {
+  x[i] <- 2*i
+  y[i] <- x[i] - 2 
+}
+
+# Vectorized code
+x <- 2*(1:10)
+y <- x - 2
+
